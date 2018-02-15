323c3476590c07e3deae910fce1ed94ed6b80775
Quentin <quentin.lenglin@gmail.com>
2018-02-15 09:59:40 +0100

    inté terminée .php

---
+++ Halan/index.php
@@ -0,0 +1,164 @@
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="img/favicon.png">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/font.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="scripts/particles.min.js"></script>
+  <title>Halan</title>
+</head>
+
+<body>
+  <div id="particles-js"></div>
+
+  <header>
+    <div class="headerContainer">
+      <div class="logoContainer">
+        <a href="index.html">
+          <img class="logoImg" src="img/logo_gradient_Gradient.svg" alt="Logo Halan">
+        </a>
+      </div>
+
+      <div class="menuContainer">
+        <a class="menuItem-link" href="">destinations</a>
+        <a class="menuItem-link" href="">about</a>
+      </div>
+    </div>
+  </header>
+
+  <div class="mainContainer">
+    <div class="mainContainer-content">
+      <h1 class="mainTitle">Safely going, beyond infinite.</h1>
+      <div class="mainDescription">
+        <p class="mainDescription-text">
+          Halan is a space travel company providing the ultimate experience for outer space travel with feature destinations such as the Moon, Mars or Europa. One of our top features is our cheeky customer service and speed - we can get you anywhere safely in our universe within 1 day.
+        </p>
+      </div>
+      <img class="backgroundImg" src="img/bg-image.png" alt="oops">
+      <form>
+        <input class="startButton" type="button" value="Book your adventure" onclick="window.location.href='planets.html'" />
+      </form>
+    </div>
+    <!-- <div class="bgCircle"></div>
+    <div class="bgCircle"></div> -->
+  </div>
+
+  <script type="text/javascript">
+    particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 355,
+          "density": {
+            "enable": true,
+            "value_area": 789.1476416322727
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.48927153781200905,
+          "random": false,
+          "anim": {
+            "enable": true,
+            "speed": 0.2,
+            "opacity_min": 0,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 2,
+          "random": true,
+          "anim": {
+            "enable": true,
+            "speed": 2,
+            "size_min": 0,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 0.2,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "bubble"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "push"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 83.91608391608392,
+            "size": 1,
+            "duration": 3,
+            "opacity": 1,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 200,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+    });
+  </script>
+</body>
+
+</html>
